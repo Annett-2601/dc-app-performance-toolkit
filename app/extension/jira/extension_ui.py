3406c51a4dc3c60d2ffe89ff30d20330fc19ef8c
Hanna Rauhen <79392656+Annett-2601@users.noreply.github.com>
2022-09-16 09:18:25 +0300

    Update extension_ui.py

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -42,13 +42,13 @@ def app_specific_action(webdriver, datasets):
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
             
             # input data into username field(admin)
-   page.wait_until_presence_of_element_located((By.ID, "login-form-username"))
+            page.wait_until_presence_of_element_located((By.ID, "login-form-username"))
             page.get_element((By.ID, "login-form-username")).send_keys("admin")
             attr_username = page.get_element((By.ID, "login-form-username")).get_attribute("value")
             assert attr_username == "admin"
 
             # input data into password field(admin)
-   page.wait_until_presence_of_element_located((By.ID, "login-form-password"))         
+            page.wait_until_presence_of_element_located((By.ID, "login-form-password"))         
             page.get_element((By.ID, "login-form-password")).send_keys("admin")
             attr_password = page.get_element((By.ID, "login-form-password")).get_attribute("value")
             assert attr_password == "admin"
