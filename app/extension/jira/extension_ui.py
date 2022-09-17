e7e3418ee21667c29e6b7ddd44aa4834b9bfa1a9
Hanna Rauhen <79392656+Annett-2601@users.noreply.github.com>
2022-09-17 12:38:00 +0300

    Update extension_ui.py

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -19,7 +19,7 @@ def app_specific_action(webdriver, datasets):
 #     NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
 #     just before test_2_selenium_z_log_out action
     
-@print_timing("selenium_app_specific_user_login")
+    @print_timing("selenium_app_specific_user_login")
     def measure():
         def app_specific_user_login(username='admin', password='admin'):
             login_page = Login(webdriver)
@@ -27,18 +27,18 @@ def app_specific_action(webdriver, datasets):
             login_page.go_to()
             login_page.set_credentials(username=username, password=password)
             if login_page.is_first_login():
-                login_page.first_login_setup()
+               login_page.first_login_setup()
             if login_page.is_first_login_second_page():
-                login_page.first_login_second_page_setup()
+               login_page.first_login_second_page_setup()
             login_page.wait_for_page_loaded()
         app_specific_user_login(username='admin', password='admin')
-    measure()
+     measure()
 
-    @print_timing("selenium_app_custom_action")
-    def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
+     @print_timing("selenium_app_custom_action")
+     def measure():
+         @print_timing("selenium_app_custom_action:view_issue")
+         def sub_measure():
+             page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
             
             # account login
             # input data into username field(admin)
